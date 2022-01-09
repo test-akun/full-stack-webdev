5beb83699a531fc1204bed97d54b06d1ab200153
test-akun <alvid.ardato@gmail.com>
2022-01-09 20:42:56 +0000

    Add PATCH endpoint - update text.

--- src/routes/todos/[uid].json.ts
+++ src/routes/todos/[uid].json.ts
@@ -4,3 +4,10 @@ import { api } from "./_api";
 export const del: RequestHandler = (request) => {
     return api(request)
 }
+
+export const patch: RequestHandler<{}, FormData> = (request) => {
+    return api(request, {
+      text: request.body.get("text"),
+    //   done: request.body.has("done") ? !!request.body.get("done") : undefined
+    });
+  }
